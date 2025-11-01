96be4a0586ff28af1b1ae2c9b97900215e6f009a
Erixk12 <erick.lopez6523@alumnos.udg.mx>
2025-11-01 09:22:58 -0600

    FIX

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ usar_linter = input("¿Quieres análisis de código estático? (S/n): ") or "s"
 steps_linter = ''
 linter_pkg = ''
 if usar_linter.lower() == 's':
-    linter = input("¿Qué herramienta usarás para código estático?: ") or "flake8"
+    linter = input("¿Qué herramienta para cód estático?: ") or "flake8"
     linter_pkg = linter.split()[0]
-    cmd_linter = input(f"Comando para análisis estático (por defecto: '{linter} .'): ") or f"{linter} ."
+    cmd_linter = input(f"Comando (por defecto: '{linter} .'): ") or f"{linter} ."
     steps_linter = f"""
     - name: Instalar herramienta de análisis estático
       run: pip install {linter_pkg}
@@ -36,7 +36,7 @@ security_pkg = ''
 if usar_security.lower() == 's':
     security = input("¿Herramienta para análisis de seguridad: ") or "bandit"
     security_pkg = security.split()[0]
-    cmd_security = input(f"Comando para análisis de seguridad (por defecto: '{security} .'): ") or f"{security} ."
+    cmd_security = input(f"Comando para (por defecto: '{security} .'): ") or f"{security} ."
     steps_security = f"""
     - name: Instalar herramienta de análisis de seguridad
       run: pip install {security_pkg}
